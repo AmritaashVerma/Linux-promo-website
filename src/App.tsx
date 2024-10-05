3e28ed2dde5763902fede62d5795d33790d2ec70
Amritaash Verma <amritaash.verma@gmail.com>
2024-10-05 16:51:30 +0530

    few changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <main className="flex min-h-screen items-center justify-center gap-2 dark:bg-gray-800">
       <h1 className="text-2xl dark:text-white">
-        Flowbite React + Create React App
+        Linux promotion website
       </h1>
       <DarkThemeToggle />
     </main>
